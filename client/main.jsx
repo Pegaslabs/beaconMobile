0e2292e41548fdb2f85d04cee866f1d2a515fea5
cookiecooker <ningjm.here@gmail.com>
2016-06-12 16:08:33 -0400

    shake detect

--- client/main.jsx
+++ client/main.jsx
@@ -5,8 +5,19 @@ import { render } from 'react-dom';
 
 import '../imports/startup/accounts-config.js'
 import App from '../imports/ui/App.jsx';
+import shake from '../imports/api/shake.js'
+
+function onDeviceReady() {
+    console.log(navigator.accelerometer);
+}
+
+function onShake() {
+    ibeacon.showMessage("shaked");
+}
 
 Meteor.startup(() => {
     ibeacon.showMessage("It works!!!");
+    document.addEventListener("deviceready", onDeviceReady, false);
+    shake.startWatch(onShake, 30);
     render(<App />, document.getElementById('render-targer'));
 });
