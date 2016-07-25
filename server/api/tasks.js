6072a8e39a649aaa9c38c6fdcf065ede6baaa2be
cookiecooker <ningjm.here@gmail.com>
2016-07-25 14:55:05 -0400

    move collections defination to single folder

--- server/api/tasks.js
+++ server/api/tasks.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
 
-import { Tasks, Logins, currentUser } from '../../collections/collection.js';
+import { Tasks, Logins, currentUser } from '../../collections/collections.js';
 
 //let database;
 
